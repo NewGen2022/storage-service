0d561c2b7b375faa58a1a00abe51a60a65e878fc
NewGen2022 <amirgimari@gmail.com>
2024-09-21 12:35:42 +0300

    added function for password hashing

---
+++ public/js/hashPassword.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcryptjs');
+
+const hashPassword = async (password) => {
+    try {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        return hashedPassword;
+    } catch (error) {
+        console.error('Error hashing password:', error);
+        throw error;
+    }
+};
+
+module.exports = hashPassword;
