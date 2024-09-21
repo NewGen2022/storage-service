6ee56f52b2c0b41fad97fd51abe0f48f23a62819
NewGen2022 <amirgimari@gmail.com>
2024-09-21 21:13:24 +0300

    added middlewares: session, passport, flash

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const expressSession = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const prisma = require('./db/prismaClient');
+const passport = require('./config/passport');
+const flash = require('connect-flash');
 const authRouter = require('./routes/auth');
 
 const PORT = process.env.PORT;
-
 const app = express();
 
 // set templating language to ejs
@@ -16,6 +20,30 @@ app.engine('ejs', ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// session middleware
+app.use(
+    expressSession({
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+        },
+        secret: process.env.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: true,
+        store: new PrismaSessionStore(prisma, {
+            checkPeriod: 2 * 60 * 1000,
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }),
+    })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Flash middleware
+app.use(flash());
 
 // routes
 app.use('/', authRouter);
