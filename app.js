fc529bbde8ebafa60daa81ccf3409981757924b8
NewGen2022 <amirgimari@gmail.com>
2024-09-21 12:27:44 +0300

    added new router: auth

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const authRouter = require('./routes/auth');
 
 const PORT = process.env.PORT;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('File uploader init ');
-});
+// set templating language to ejs
+app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate);
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use('/', authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
