c7bb1df4b05f2f42ca0c4f5eea0b11ffde93ee52
NewGen2022 <amirgimari@gmail.com>
2024-09-22 18:50:54 +0300

    added new route: /directory, new render page at / route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { isAuth } = require('../middleware/authMidware');
 
-router.get('/', isAuth, (req, res) => {
+router.get('/', (req, res) => {
+    res.render('welcome_page');
+});
+
+router.get('/directory', isAuth, (req, res) => {
     res.render('index', { user: req.user });
 });
 
