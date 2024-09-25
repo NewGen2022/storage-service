d718f224a06d202a2c24c57cec85287c9bee6c93
NewGen2022 <amirgimari@gmail.com>
2024-09-25 15:34:38 +0300

    added: post route to create dir, get for any directory id; changed: get directory route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const { isAuth } = require('../middleware/authMidware');
-const { welcomePage, indexRender } = require('../controllers/indexController');
+const {
+    welcomePage,
+    indexRender,
+    addDir,
+} = require('../controllers/indexController');
+const { getRootDir } = require('../db/queries');
 
+// GET ROUTES
 router.get('/', welcomePage);
 
-router.get('/directory', isAuth, indexRender);
+router.get('/directory', isAuth, async (req, res) => {
+    const rootDir = await getRootDir();
+    res.redirect(`/directory/${rootDir.id}`);
+});
+
+router.get('/directory/:dirId', isAuth, indexRender);
+
+// POST ROUTES
+router.post('/directory/create/dir', isAuth, addDir);
 
 module.exports = router;
