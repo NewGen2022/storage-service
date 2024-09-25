e68fce61b45507c158607fb714fc3a36c9cc4835
NewGen2022 <amirgimari@gmail.com>
2024-09-25 16:54:32 +0300

    added function to show form to add file

--- public/js/showForms.js
+++ public/js/showForms.js
@@ -1,15 +1,29 @@
-const addDirForm = () => {
+const showAddDirForm = () => {
     const addDirForm = document.getElementById('addDirForm');
     const addDirBtn = document.getElementById('addDirBtn');
-    const cancelBtn = document.getElementById('cancelBtn');
+    const cancelDirBtn = document.getElementById('cancelDirBtn');
 
     addDirBtn.addEventListener('click', () => {
         addDirForm.style.display = 'flex';
     });
 
-    cancelBtn.addEventListener('click', () => {
+    cancelDirBtn.addEventListener('click', () => {
         addDirForm.style.display = 'none';
     });
 };
 
-export { addDirForm };
+const showAddFileForm = () => {
+    const addFileForm = document.getElementById('addFileForm');
+    const addFileBtn = document.getElementById('addFileBtn');
+    const cancelFileBtn = document.getElementById('cancelFileBtn');
+
+    addFileBtn.addEventListener('click', () => {
+        addFileForm.style.display = 'flex';
+    });
+
+    cancelFileBtn.addEventListener('click', () => {
+        addFileForm.style.display = 'none';
+    });
+};
+
+export { showAddDirForm, showAddFileForm };
