48d2e731e2ee475bb3534176fe69e353a2c351e7
NewGen2022 <amirgimari@gmail.com>
2024-09-25 15:38:18 +0300

    added controller to handle new dir creation

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,4 @@
-const { getAllDirectories } = require('../db/queries');
+const { getAllUserDirectories, createDirectory } = require('../db/queries');
 
 const welcomePage = (req, res) => {
     if (req.isAuthenticated()) {
@@ -9,8 +9,39 @@ const welcomePage = (req, res) => {
 };
 
 const indexRender = async (req, res) => {
-    const dirs = await getAllDirectories();
-    res.render('index', { user: req.user, dirs: dirs });
+    try {
+        const userId = req.user.id;
+        const dirs = await getAllUserDirectories(userId);
+        const currentDirId = req.params.dirId || null;
+
+        res.render('index', {
+            user: req.user,
+            dirs: dirs,
+            currentDirId: currentDirId,
+        });
+    } catch (error) {
+        console.error('Error fetching user directories:', error);
+        req.flash(
+            'error',
+            'Unable to load directories. Please try again later.'
+        );
+        res.redirect('/directory'); // Redirect to a safe route
+    }
 };
 
-module.exports = { welcomePage, indexRender };
+const addDir = async (req, res) => {
+    const dirName = req.body.dirName;
+    const userId = req.user.id;
+    const parentId = req.body.parentId;
+
+    try {
+        const newDir = await createDirectory(dirName, userId, parentId);
+        res.redirect(`/directory/${newDir.id}`);
+    } catch (error) {
+        console.error('Error creating directory:', error);
+        req.flash('error', 'Failed to create directory. Please try again.');
+        res.redirect('/directory');
+    }
+};
+
+module.exports = { welcomePage, indexRender, addDir };
