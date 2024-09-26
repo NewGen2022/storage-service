9dde4a44cd4361dbbdf6152043aa81d78891e773
NewGen2022 <amirgimari@gmail.com>
2024-09-26 14:05:57 +0300

    added function with detailed time and date

--- public/js/timeFormatting.js
+++ public/js/timeFormatting.js
@@ -1,9 +1,16 @@
 const options = {
     year: 'numeric',
     day: 'numeric',
+    month: 'numeric',
+    hour12: false,
+};
+
+const optionsDetailed = {
+    year: 'numeric',
+    day: 'numeric',
     month: 'long',
-    // hour: 'numeric',
-    // minute: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
     hour12: false,
 };
 
@@ -11,4 +18,8 @@ const formatDate = (date) => {
     return date.toLocaleString('en-GB', options);
 };
 
-module.exports = { formatDate };
+const formatDateDetailed = (date) => {
+    return date.toLocaleString('en-GB', optionsDetailed);
+};
+
+module.exports = { formatDate, formatDateDetailed };
