da2df18561b3b8d94127c068772ccadac9ef1d67
NewGen2022 <amirgimari@gmail.com>
2024-10-10 12:02:34 +0300

    added controller to handle creation of share link for directory

--- controllers/shareController.js
+++ controllers/shareController.js
@@ -1,9 +1,33 @@
-const shareFile = async (req, res) => {};
+const { calculateExpireAt } = require('../public/js/timeFormatting');
+const { createShareDirLink } = require('../db/queries');
 
-const shareDir = async (req, res) => {
-    const dirId = req.params.dirId;
-    const userId = req.user.id;
-    const duration = req.body.duration;
+// SHOW SHARE LINK CONTROLLERS
+const showSharedFileController = async (req, res) => {};
+
+const showSharedDirController = async (req, res) => {
+    res.render('sharedDir');
 };
 
-module.exports = { shareFile, shareDir };
+// CREATE SHARE LINK CONTROLLERS
+const shareFileController = async (req, res) => {};
+
+const shareDirController = async (req, res) => {
+    const dirId = req.params.dirId;
+    const duration = req.body.duration;
+    const expiresAt = calculateExpireAt(Number(duration));
+
+    try {
+        const shareDirLink = await createShareDirLink(dirId, expiresAt);
+        res.status(201).json(shareDirLink);
+    } catch (err) {
+        console.error('Error creating share link for directory:', err);
+        req.flash('error', 'Error creating share link for directory');
+    }
+};
+
+module.exports = {
+    showSharedFileController,
+    showSharedDirController,
+    shareFileController,
+    shareDirController,
+};
